d3c1dc320fb40d75d4d627f7396c0e1550a57c29
FelixHarris <100237106+FelixHarris@users.noreply.github.com>
2022-08-05 15:28:02 +1200

    Roiley

--- Godot/Scripts/SpinningBlade.gd
+++ Godot/Scripts/SpinningBlade.gd
@@ -1,8 +1,13 @@
 extends Node2D
+export (int) var follow_speed = 50
 
 
 func _ready():
-	$AnimationPlayer.play("Swing")
+	$AnimationPlayer.play("Spin")
+
+func _process(delta):
+	$Path2D/PathFollow2D.offset += follow_speed * delta
+
 
 
 
