7740d0de240797fabcf5a02792474a08bee26b6d
FelixHarris <felhar75@kingshigh.school.nz>
2022-08-12 12:21:43 +1200

    Riole

---
+++ Godot/Sound.gd
@@ -0,0 +1,18 @@
+extends Node
+
+onready var music = AudioStreamPlayer.new()
+
+
+var music_tracks = {
+	"title_track":"res:"
+}
+
+var music_db = 1
+var sound_db = 1
+
+func change_music_db
+
+
+
+func _ready():
+	pass
