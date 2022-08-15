2e4160b63a56923d066bbb7fee39e71b51819def
FelixHarris <felhar75@kingshigh.school.nz>
2022-08-15 13:23:07 +1200

    Roiiileeee

--- Godot/Sound.gd
+++ Godot/Sound.gd
@@ -10,9 +10,27 @@ var music_tracks = {
 var music_db = 1
 var sound_db = 1
 
-func change_music_db
+func change_music_db(val):
+	music_db = linear2db(val)
+	
+func change_sound_db(val):
+	sound_db = linear2db(val)
+	
 
 
 
 func _ready():
-	pass
+	music.stream = load(music_tracks["main"])
+	add_child(music)
+	music.play()
+	print(music.stream)
+	print("playing song")
+
+func play_sound_effect(sfx):
+	var sound = AudioStreamPlayer.new()
+	sound.stream = load(sound_effects[sfx])
+	add_child(sound)
+	sound.play(
+		yield(sound,"finished")
+	)
+	
