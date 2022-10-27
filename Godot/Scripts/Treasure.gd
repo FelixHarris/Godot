b6fed1952cd02bfcf918cae2db858a2a17aae0ab
FelixHarris <100237106+FelixHarris@users.noreply.github.com>
2022-10-27 19:11:04 +1300

    finished game

--- Godot/Scripts/Treasure.gd
+++ Godot/Scripts/Treasure.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 
 func _ready():
-	pass
+	$AnimationPlayer.play("Idle")
 
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene("res://Scenes/T)
+		get_tree().change_scene("res://Scenes/Winning screen.tscn")
